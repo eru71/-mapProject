3123a527ee4bc3f6183207d67550b84ab75e15fd
eru71 <colette71@foxmail.com>
2015-11-06 08:42:31 +0800

    map功能实现，还需要大头针

--- 美食map/美食map/ViewController.m
+++ 美食map/美食map/ViewController.m
@@ -7,28 +7,47 @@
 //
 
 #import "ViewController.h"
-#import <BaiduMapAPI_Map/BMKMapView.h>
+
 
 @interface ViewController ()<BMKMapViewDelegate>
-@property(nonatomic,strong) BMKMapView* mapView;
+@property (nonatomic,strong) BMKMapView* mapView;
+@property (nonatomic,strong) CLLocationManager *manager;
 @end
 @implementation ViewController
+
+-(CLLocationManager *)manager{
+    if (!_manager) {
+        _manager = [CLLocationManager new];
+        if ([UIDevice currentDevice].systemVersion.floatValue>8.0) {
+            [_manager requestAlwaysAuthorization];
+        }
+    }
+    return _manager;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     _mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)];
     self.view = _mapView;
+    _mapView.userTrackingMode = BMKUserTrackingModeFollow;
 }
 -(void)viewWillAppear:(BOOL)animated
 {
     [_mapView viewWillAppear];
     _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
 }
+
 -(void)viewWillDisappear:(BOOL)animated
 {
     [_mapView viewWillDisappear];
     _mapView.delegate = nil; // 不用时，置nil
+}
+
+-(void)updateLocationData:(BMKUserLocation *)userLocation{
+    
+    BMKCoordinateSpan span = {0.01,0.01};
+    BMKCoordinateRegion region = {userLocation.location,span};
 }
 
 - (void)didReceiveMemoryWarning {
